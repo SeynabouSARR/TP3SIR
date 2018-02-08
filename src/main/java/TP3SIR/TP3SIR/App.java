b40b926daf0dc1e4d4d875afefbd1bd7bbbd3736
Seynabou SARR <seynabousarr90@yahoo.fr>
2018-02-08 17:43:09 +0100

    Initial commit

---
+++ src/main/java/TP3SIR/TP3SIR/App.java
@@ -0,0 +1,13 @@
+package TP3SIR.TP3SIR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
