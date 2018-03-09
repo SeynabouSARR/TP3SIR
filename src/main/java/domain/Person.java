04533ab7baa7e27b6866ea79b2c357b1057c448a
Melaine BOUE <melaineboue@gmail.com>
2018-03-09 09:13:45 +0100

    checkstyle Principal and Person done

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -11,53 +11,52 @@ import org.mongodb.morphia.annotations.Reference;
 
 @Entity("person")
 public class Person {
-	@Id
-	@Property("id")
-	private ObjectId id;
-	private String name;
-	@Reference
-	private List<Address> address = new ArrayList<Address>();
+  @Id
+  @Property("id")
+  private ObjectId id;
+  private String name;
+  @Reference
+  private List<Address> address = new ArrayList<Address>();
 
-	public Person() {
+  public Person() {
+  }
 
-	}
+  public Person(ObjectId id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-	public Person(ObjectId id, String name) {
-		this.id =id ;
-		this.name = name;
-	}
+  public ObjectId getId() {
+    return id;
+  }
 
-	public ObjectId getId() {
-		return id;
-	}
+  public void setId(ObjectId id) {
+    this.id = id;
+  }
 
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public List<Address> getAddress() {
+    return address;
+  }
 
-	public List<Address> getAddress() {
-		return address;
-	}
+  public void setAddress(List<Address> address) {
+    this.address = address;
+  }
 
-	public void setAddress(List<Address> address) {
-		this.address = address;
-	}
+  public void addAddress(Address address) {
+    this.address.add(address);
+  }
 
-	public void addAddress(Address address) {
-		this.address.add(address);
-	}
-
-	@Override
-	public String toString() {
-		return "Person [id=" + this.id + "] , name : " + this.name + ", " + this.address ;
-	}
+  @Override
+  public String toString() {
+    return "Person [id=" + this.id + "] , name : " + this.name + ", " + this.address;
+  }
 
 }
--- src/main/java/domain/Principal.java
+++ src/main/java/domain/Principal.java
@@ -15,84 +15,76 @@ public class Principal {
    */
   public static void main(String[] args) {
 
-    ArticleDAO managerArticle = new ArticleDAO(DATABASE_NAME);
+    Address addressBoisPerrin11 = new Address();
+    addressBoisPerrin11.setStreet("11 Rue du bois perrin");
+    addressBoisPerrin11.setCity("Rennes");
+    addressBoisPerrin11.setPostCode("35700");
+    addressBoisPerrin11.setCountry("France");
 
+    Address addressBoisPerrin13 = new Address();
+    addressBoisPerrin13.setStreet("13A Rue du bois perrin");
+    addressBoisPerrin13.setCity("Rennes");
+    addressBoisPerrin13.setPostCode("35700");
+    addressBoisPerrin13.setCountry("France");
 
-	    Address address_bois_perrin11 = new Address();
-	    address_bois_perrin11.setStreet("11 Rue du bois perrin");
-	    address_bois_perrin11.setCity("Rennes");
-	    address_bois_perrin11.setPostCode("35700");
-	    address_bois_perrin11.setCountry("France");
+    Address addressCessonSevigne = new Address();
+    addressCessonSevigne.setStreet("70 Rue de Rennes");
+    addressCessonSevigne.setCity("Cesson Sevigné");
+    addressCessonSevigne.setPostCode("35510");
+    addressCessonSevigne.setCountry("France");
 
-	    Address address_bois_perrin13 = new Address();
-	    address_bois_perrin13.setStreet("13A Rue du bois perrin");
-	    address_bois_perrin13.setCity("Rennes");
-	    address_bois_perrin13.setPostCode("35700");
-	    address_bois_perrin13.setCountry("France");
-
-	    Address address_cesson_sevigne = new Address();
-	    address_cesson_sevigne.setStreet("70 Rue de Rennes");
-	    address_cesson_sevigne.setCity("Cesson Sevigné");
-	    address_cesson_sevigne.setPostCode("35510");
-	    address_cesson_sevigne.setCountry("France");
-
-	    //Key<Address> cle1 = ds.save(address_bois_perrin11);
+    //Key<Address> cle1 = ds.save(address_bois_perrin11);
 
     AddressDAO managerAddress = new AddressDAO(DATABASE_NAME);
-    managerAddress.save(address_bois_perrin11);
-    managerAddress.save(address_bois_perrin13);
-    managerAddress.save(address_cesson_sevigne);
+    managerAddress.save(addressBoisPerrin11);
+    managerAddress.save(addressBoisPerrin13);
+    managerAddress.save(addressCessonSevigne);
     System.out.println("Enregistrement des adresses terminées");
 
     Person seynabou = new Person();
     seynabou.setName("Seynabou SARR");
-	    Person melaine = new Person();
-	    melaine.setName("Melaine BOUE");
-	    Person meco = new Person();
-	    meco.setName("Meco DEBOUE");
+    Person melaine = new Person();
+    melaine.setName("Melaine BOUE");
+    Person meco = new Person();
+    meco.setName("Meco DEBOUE");
 
-	    melaine.addAddress(address_bois_perrin11);
-	    seynabou.addAddress(address_bois_perrin13);
-	    meco.addAddress(address_cesson_sevigne);
+    melaine.addAddress(addressBoisPerrin11);
+    seynabou.addAddress(addressBoisPerrin13);
+    meco.addAddress(addressCessonSevigne);
 
-	    PersonDAO managerPerson = new PersonDAO(DATABASE_NAME);
-	    managerPerson.save(melaine);
-	    managerPerson.save(seynabou);
-	    managerPerson.save(meco);
-	    System.out.println("Enregistrement des personnes terminées");
+    PersonDAO managerPerson = new PersonDAO(DATABASE_NAME);
+    managerPerson.save(melaine);
+    managerPerson.save(seynabou);
+    managerPerson.save(meco);
+    System.out.println("Enregistrement des personnes terminées");
 
-	    Article myHpComputer = new Article();
-	    myHpComputer.setStars(5);
-	    myHpComputer.setName("HP LAPTOP OIE");
-	    myHpComputer.addBuyer(seynabou);
-	    managerArticle.save(myHpComputer);
+    Article myHpComputer = new Article();
+    myHpComputer.setStars(5);
+    myHpComputer.setName("HP LAPTOP OIE");
+    myHpComputer.addBuyer(seynabou);
+    ArticleDAO managerArticle = new ArticleDAO(DATABASE_NAME);
+    managerArticle.save(myHpComputer);
 
-	    Article telephone_iphone = new Article();
-	    telephone_iphone.setStars(5);
-	    telephone_iphone.setName("iPhone 7S");
-	    telephone_iphone.addBuyer(seynabou);
-	    telephone_iphone.addBuyer(melaine);
-	    telephone_iphone.addBuyer(meco);
-	    managerArticle.save(telephone_iphone);
+    Article telephoneIphone = new Article();
+    telephoneIphone.setStars(5);
+    telephoneIphone.setName("iPhone 7S");
+    telephoneIphone.addBuyer(seynabou);
+    telephoneIphone.addBuyer(melaine);
+    telephoneIphone.addBuyer(meco);
+    managerArticle.save(telephoneIphone);
 
-	    System.out.println("Enregistrement des articles");
+    System.out.println("Enregistrement des articles");
+    //Article article_a_effacer = managerArticle.find("iPhone 7S");
+    //managerArticle.delete(article_a_effacer);
+    List<Article> articles = managerArticle.getList();
 
-
-
-		//Article article_a_effacer = managerArticle.find("iPhone 7S");
-		//managerArticle.delete(article_a_effacer);
-	    List<Article> articles = managerArticle.getList();
-
-	    System.out.println("--------------------------------------------------------");
-	    int compteur = 1;
-	    for (Article article : articles) {
-			System.out.println(compteur+" "+article);
-			compteur++;
-		}
-	    System.out.println("--------------------------------------------------------");
-
-
-
-	}
+    System.out.println("--------------------------------------------------------");
+    int compteur = 1;
+    for (Article article : articles) {
+      System.out.println(compteur + " " + article);
+      compteur++;
+    }
+    System.out.println("--------------------------------------------------------");
+  }
 
 }
